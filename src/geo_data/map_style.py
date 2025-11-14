3e2c33cd5cfda72ed446258e7fe48d5ad08ef544
akuehn <adrian-kuehn@posteo.de>
2025-11-14 15:15:18 +0100

    Make colors and styles immutable
    The colors and styles are loaded from a file and should not be changeable. Therefore, instead of using `dict`, `MappingProxyType` is now used, which makes kind of immutable dicts (not directly, but it fits the purpose).

--- src/geo_data/map_style.py
+++ src/geo_data/map_style.py
@@ -5,8 +5,9 @@ They are loaded from a config file and define colors and styles for geographical
 elements like land, sea, etc.
 """
 
+from types import MappingProxyType
+
 import yaml
-from matplotlib.typing import ColorType
 
 from geo_data import helpers
 
@@ -15,13 +16,17 @@ CONFIG_PATH = helpers.get_top_directory() / "data" / "map_style.yaml"
 with CONFIG_PATH.open() as f:
     config = yaml.safe_load(f)
 
-# get colors from config
-COLORS: dict[str, ColorType] = config["colors"]
+# get colors from config and create immutable dict
+COLORS: MappingProxyType[str, str] = MappingProxyType(config["colors"])
 
 # get styles from config and resolve colors
 # (e.g. if in styles "fill: land", land is replaced by "#fefee9")
-STYLES: dict[str, dict] = config["styles"]
-for _, kwargs in STYLES.items():
+_styles = config["styles"]
+for style_name, kwargs in _styles.items():
     for key, value in kwargs.items():
         if value in COLORS.keys():
             kwargs[key] = COLORS[value]
+    _styles[style_name] = MappingProxyType(kwargs)
+
+# immutable dict
+STYLES: MappingProxyType[str, MappingProxyType[str, str]] = MappingProxyType(_styles)
