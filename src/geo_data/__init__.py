e1ed76641c53c8d03fdb22d7101c4d0d49519d39
akuehn <adrian-kuehn@posteo.de>
2026-06-06 00:55:39 +0200

    Fill package __init__.py

--- src/geo_data/__init__.py
+++ src/geo_data/__init__.py
@@ -1,1 +1,14 @@
 """Provide a tool kit to work with data of geographical features and visualize them."""
+
+__version__ = "0.1.0"
+
+from . import data, data_handler, helpers, map_style, projections, svg_handler
+
+__all__ = [
+    "data",
+    "data_handler",
+    "helpers",
+    "map_style",
+    "projections",
+    "svg_handler",
+]
