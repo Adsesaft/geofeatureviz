83e6ae5c6adf621c4e55fe69a39b7f9abe68fac5
akuehn <adrian-kuehn@posteo.de>
2026-06-06 00:39:17 +0200

    Move loading functions into new module loader.py
    The functions for loading the country translations and regional groups are moved into a new module.

--- src/geo_data/data/loader.py
+++ src/geo_data/data/loader.py
@@ -1,1 +1,46 @@
 """Load datasets and other files."""
+
+from typing import TypedDict, cast
+
+import pandas as pd
+import yaml
+
+from geo_data.data.config import settings
+
+
+def get_country_translations() -> pd.DataFrame:
+    """Get a dataframe containing translations of countries.
+
+    Returns:
+        A DataFrame currently containing 3 columns:
+        - code: The ISO 3166 alpha 3 country code (3 letter unique country id)
+        - german: The German name of the country (consistent with Anki Ultimate
+                  Geography, which is consistent with German Wikipedia.)
+        - english: The English name of the country (I didn't investigate further).
+    """
+    df = pd.read_csv(settings.country_translation_path)
+    return df
+
+
+class Region(TypedDict):
+    """Provides the structure for regional groups read from the yaml file."""
+
+    core: list[str]
+    optional: list[str]
+    continent: list[str]
+    projection: str
+
+
+def get_regional_groups() -> dict[str, Region]:
+    """Add function to read in regional groups.
+
+    Returns:
+        A dictionary with the regional group names as key. The values are dictionaries
+        with the keys:
+        - "core": List of the core countries of the region.
+        - "optional": List of optional countries of the region.
+        - "continent": List of continents on which the region is located.
+    """
+    with open(settings.regional_groups_path, "r", encoding="utf-8") as f:
+        regions = yaml.safe_load(f)
+    return cast(dict[str, Region], regions)
