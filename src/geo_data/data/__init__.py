d2221e792b9cca265c3d3945c0b10dd45edc5406
akuehn <adrian-kuehn@posteo.de>
2026-06-06 01:07:14 +0200

    Fill __init__.py of data package
    Especially, path_settings are imported from the config module.

--- src/geo_data/data/__init__.py
+++ src/geo_data/data/__init__.py
@@ -1,1 +1,6 @@
 """Provide tools to load and process data."""
+
+from . import config, datasets, loader
+from .config import path_settings
+
+__all__ = ["config", "datasets", "loader", "path_settings"]
