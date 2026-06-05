e8d7fc05e4642083dfc2c91c834ff86000d9a1c1
akuehn <adrian-kuehn@posteo.de>
2026-06-06 00:39:17 +0200

    Move loading functions into new module loader.py
    The functions for loading the country translations and regional groups are moved into a new module.

--- src/geo_data/data_handler.py
+++ src/geo_data/data_handler.py
@@ -3,27 +3,17 @@
 import shutil
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Generator, TypedDict, cast
+from typing import Generator
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-import yaml
 from anki.collection import Collection
 from shapely.geometry.multipolygon import MultiPolygon
 from shapely.geometry.polygon import Polygon
 
 from geo_data.data import datasets
 from geo_data.data.config import settings
-
-
-class Region(TypedDict):
-    """Provides the structure for regional groups read from the yaml file."""
-
-    core: list[str]
-    optional: list[str]
-    continent: list[str]
-    projection: str
 
 
 def load(
@@ -226,32 +216,3 @@ def anki_to_df(deck_name: str) -> pd.DataFrame:
             result["NoteType"] = note_type_name
             notes_with_type.append(result)
     return pd.DataFrame(notes_with_type)
-
-
-def get_regional_groups() -> dict[str, Region]:
-    """Add function to read in regional groups.
-
-    Returns:
-        A dictionary with the regional group names as key. The values are dictionaries
-        with the keys:
-        - "core": List of the core countries of the region.
-        - "optional": List of optional countries of the region.
-        - "continent": List of continents on which the region is located.
-    """
-    with open(settings.regional_groups_path, "r", encoding="utf-8") as f:
-        regions = yaml.safe_load(f)
-    return cast(dict[str, Region], regions)
-
-
-def get_country_translations() -> pd.DataFrame:
-    """Get a dataframe containing translations of countries.
-
-    Returns:
-        A DataFrame currently containing 3 columns:
-        - code: The ISO 3166 alpha 3 country code (3 letter unique country id)
-        - german: The German name of the country (consistent with Anki Ultimate
-                  Geography, which is consistent with German Wikipedia.)
-        - english: The English name of the country (I didn't investigate further).
-    """
-    df = pd.read_csv(settings.country_translation_path)
-    return df
