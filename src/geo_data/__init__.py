ecebc1d6770be1775114cb88bdc63da44ad948ac
akuehn <adrian-kuehn@posteo.de>
2026-06-06 02:15:28 +0200

    Move preprocessor into data package

--- src/geo_data/__init__.py
+++ src/geo_data/__init__.py
@@ -2,7 +2,8 @@
 
 __version__ = "0.1.0"
 
-from . import data, helpers, map_style, preprocessor, projections, svg_handler
+from . import data, helpers, map_style, projections, svg_handler
+from .data import preprocessor
 
 __all__ = [
     "data",
