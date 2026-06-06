82fd7d4c4246d14d0fc2728b6b4b3d9e31a7b270
akuehn <adrian-kuehn@posteo.de>
2026-06-06 02:15:28 +0200

    Move preprocessor into data package

--- src/geo_data/data/__init__.py
+++ src/geo_data/data/__init__.py
@@ -1,6 +1,6 @@
 """Provide tools to load and process data."""
 
-from . import config, datasets, loader
+from . import config, datasets, loader, preprocessor
 from .config import path_settings
 
-__all__ = ["config", "datasets", "loader", "path_settings"]
+__all__ = ["config", "datasets", "loader", "preprocessor", "path_settings"]
