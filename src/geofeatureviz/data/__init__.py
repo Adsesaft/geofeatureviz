b72893c194dfb4b93b28a663cc2d42c698517eac
akuehn <adrian-kuehn@posteo.de>
2026-06-23 18:33:05 +0200

    Make OverpassAPIHandler importable from geofeatureviz.data

--- src/geofeatureviz/data/__init__.py
+++ src/geofeatureviz/data/__init__.py
@@ -1,6 +1,14 @@
 """Provide tools to load and process data."""
 
 from . import config, datasets, loader, preprocessor
+from ._overpass_api_handler import OverpassAPIHandler
 from .config import path_settings
 
-__all__ = ["config", "datasets", "loader", "preprocessor", "path_settings"]
+__all__ = [
+    "config",
+    "datasets",
+    "loader",
+    "preprocessor",
+    "path_settings",
+    "OverpassAPIHandler",
+]
