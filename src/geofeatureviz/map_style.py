a0152edb3fba336839bbf49e51e1e599d8d762f4
akuehn <adrian-kuehn@posteo.de>
2026-07-07 20:47:09 +0200

    Add an ElevationColormap
    This class is a matplotlib ListedColormap, with additional features for elevations. This map can be used to get colors for creating topographic maps.

--- src/geofeatureviz/map_style.py
+++ src/geofeatureviz/map_style.py
@@ -5,10 +5,15 @@ They are loaded from a config file and define colors and styles for geographical
 elements like land, sea, etc.
 """
 
+from collections.abc import Sequence
 from types import MappingProxyType
 from typing import Any
 
+import matplotlib.colors as mplc
+import numpy as np
 import yaml
+from matplotlib.typing import ColorType
+from numpy.typing import ArrayLike, NDArray
 
 from geofeatureviz.data.config import path_settings
 
@@ -50,3 +55,58 @@ def get_topo_colors() -> dict[str, dict[int, str]]:
             topo_type, topo_level = topo_str_split[1], int(topo_str_split[2])
             topo_colors[topo_type][topo_level] = color
     return topo_colors
+
+
+class ElevationColormap(mplc.ListedColormap):
+    """Colormap of elevation levels for topographic maps."""
+
+    def __init__(
+        self,
+        colors_below: Sequence[ColorType],
+        color_sea_level: ColorType,
+        colors_above: Sequence[ColorType],
+    ) -> None:
+        """Initialize the colormap for elevation levels in a topographic map.
+
+        Args:
+            colors_below: Colors for elevation levels below sea level, in increasing
+                order of elevation.
+            color_sea_level: Color for sea level (elevation = 0).
+            colors_above: Colors for elevation levels above sea level, in increasing
+                order of elevation.
+        """
+        n_max = max(len(colors_below), len(colors_above))
+        colors_below = self._stretch_list(list(colors_below)[::-1], n_max)[::-1]
+        colors_above = self._stretch_list(list(colors_above), n_max)
+        assert len(colors_below) == len(colors_above)
+        colors = list(colors_below) + [color_sea_level] + list(colors_above)
+        super().__init__(colors, name="elevation")
+
+    def _stretch_list(self, lst: list[Any], target_len: int) -> list[Any]:
+        """Stretch a list to a target length by repeating the last element.
+
+        Args:
+            lst: The list to stretch.
+            target_len: The desired length of the list.
+
+        Returns:
+            A new list of length target_len, where the last element of lst is repeated.
+        """
+        n_missing = target_len - len(lst)
+        return list(lst) + n_missing * [lst[-1]]
+
+    def get_hex(self, values: ArrayLike) -> str | NDArray[np.str_]:
+        """Get the hex representation of the colors for the values.
+
+        Args:
+            values: Values for which the colors are requested.
+
+        Returns:
+            Colors in hex format for the given values.
+        """
+        values = np.asarray(values)
+        colors = np.asarray(super().__call__(values)).reshape((-1, 4))
+        hex_colors = [mplc.to_hex(tuple(color)) for color in colors]
+        if values.ndim == 0:
+            return hex_colors[0]
+        return np.reshape(hex_colors, values.shape)
