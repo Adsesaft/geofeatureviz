1d320fd2c861887f984e2c97d4164392cab8ff38
akuehn <adrian-kuehn@posteo.de>
2026-07-09 20:53:23 +0200

    Add an example script to create a world map

---
+++ scripts/examples/world_map.py
@@ -0,0 +1,26 @@
+"""Create a map of the world."""
+
+from geofeatureviz import preprocessor
+from geofeatureviz.data import path_settings
+from geofeatureviz.map_style import COLORS
+from geofeatureviz.svg_handler import MapSVG
+
+# load preprocessed countries and simplify the geometries
+countries = preprocessor.prep_dataset(
+    feature="country",
+    source="ne",
+    resolution=110,
+)
+countries.geometry = countries.geometry.simplify(tolerance=0.2, preserve_topology=True)
+
+# create a canvas to draw a map to
+canvas = MapSVG(width=600, height=400)
+
+# add a background covering the whole canvas, defaults to a blue ocean color
+canvas.add_background(COLORS["lake"])
+
+# draw countries in the GeoDataFrame to the map
+canvas.add_gdf(countries, "Countries", fill=COLORS["land"], stroke=COLORS["border"])
+
+# save the svg file
+canvas.save(path_settings.results_dir / "examples" / "world_map.svg", optimize=True)
