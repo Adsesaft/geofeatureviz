6a07436f475f07f7ab06805ad201f25f7bd4a066
akuehn <adrian-kuehn@posteo.de>
2026-07-09 21:34:39 +0200

    Move helpers into io-folder

--- src/geofeatureviz/io/__init__.py
+++ src/geofeatureviz/io/__init__.py
@@ -1,12 +1,13 @@
 """Provide tools to load and save data."""
 
-from . import anki_connector, datasets, loader
+from . import anki_connector, datasets, helpers, loader
 from ._overpass_api_handler import OverpassAPIHandler
 from .config import path_settings
 
 __all__ = [
     "anki_connector",
     "datasets",
+    "helpers",
     "loader",
     "OverpassAPIHandler",
     "path_settings",
