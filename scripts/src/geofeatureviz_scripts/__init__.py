2cd756fb37e7579720b18c6517008a8992a284b8
akuehn <adrian-kuehn@posteo.de>
2026-07-12 00:54:51 +0200

    Add a folder for script-modules
    I want to separate the core library geofeatureviz from utils that are only used for e.g. loading data, i.e. separating the library from pure execution stuff. Therefore, I create a sub-project, which will contain execution-focused utils.

---
+++ scripts/src/geofeatureviz_scripts/__init__.py
@@ -0,0 +1,8 @@
+"""Provide utils to run scripts using geofeatureviz.
+
+GeoFeatureViz should serve as a standalone library that provides functionality for
+visualizing geographical features. Running scripts using this library does, for example,
+not integrate things like loading data, using jupyter notebooks, or using data-specific
+configurations. The idea of this sub-project is to provide these kind of things, to
+separate the core library from data- and user-specific script-utils.
+"""
