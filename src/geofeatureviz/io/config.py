787dc598a046b3e6554f2503067b7bd8a30b9047
akuehn <adrian-kuehn@posteo.de>
2026-07-11 17:29:04 +0200

    Move country translations to resources

--- src/geofeatureviz/io/config.py
+++ src/geofeatureviz/io/config.py
@@ -75,7 +75,7 @@ class PathSettings(BaseSettings):
     @property
     def country_translation_path(self) -> Path:
         """File path to CSV-file containing country translations."""
-        return self.data_dir / "country_translations.csv"
+        return self.resources_dir / "country_translations.csv"
 
     @property
     def anki_collection_path(self) -> Path:
