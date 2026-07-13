09b1b0312db4e498cbe68f2d19868e6fbce5df54
akuehn <adrian-kuehn@posteo.de>
2026-07-13 23:47:22 +0200

    Move load_dataset into geofeatureviz_scripts

--- scripts/src/geofeatureviz_scripts/loader.py
+++ scripts/src/geofeatureviz_scripts/loader.py
@@ -2,7 +2,7 @@
 
 import geopandas as gpd
 
-from geofeatureviz.io import loader
+from geofeatureviz.io import datasets, path_settings
 from geofeatureviz.preprocessing import preprocessor
 
 
@@ -37,7 +37,43 @@ def load_and_prep_data(
     Returns:
         A GeoPandas DataFrame with the requested preprocessed geographical data.
     """
-    dataset = loader.load_dataset(feature=feature, source=source, resolution=resolution)
+    dataset = load_dataset(feature=feature, source=source, resolution=resolution)
     return preprocessor.prep_dataset(
         dataset, identifier=identifier, projection=projection
     )
+
+
+def load_dataset(
+    feature: str,
+    source: str = "ne",
+    resolution: int = 10,
+) -> gpd.GeoDataFrame:
+    """Load geographical data from a file as GeoPandas GeoDataFrame.
+
+    All available data sets can be found in the datasets module dictionary. The dataset
+    is either processed (and hence stored in `data/processed`) or raw (and hence stored
+    in `data/raw`). This function returns processed if it exists and raw if not.
+
+    Args:
+        feature: Kind of geographical feature, e.g. "country" or "river".
+        source: Data source, e.g. "ne" for NaturalEarth. Defaults to "ne".
+        resolution: Resolution of the geographical data. Defaults to 10.
+
+    Raises:
+        FileNotFoundError: If the requested file could neither be found in the processed
+            data dir nor in the raw data dir, even though it is saved in the registry.
+
+    Returns:
+        A GeoPandas DataFrame with the requested geographical data.
+    """
+    dataset_key = datasets.DatasetKey(feature, source, resolution)
+    file_name = datasets.get_dataset_filename(dataset_key)
+    file_path = path_settings.data_processed_dir / file_name
+    if not file_path.exists():
+        file_path = path_settings.data_raw_dir / file_name
+    if not file_path.exists():
+        raise FileNotFoundError(
+            f"The dataset with key {dataset_key} is saved in the registry, but the "
+            "dataset file could not be found in the files."
+        )
+    return gpd.read_file(file_path)
